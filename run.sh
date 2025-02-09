8399d9dc6dc3d7e10f2d1bf4a6783098073020c3
Jeverson Dias da Silva <jeversondiasdasilva@gmail.com>
2025-02-09 03:16:01 -0300

    Update run.sh

--- run.sh
+++ run.sh
@@ -100,15 +100,20 @@ if [ -n "$version" ]; then
 
     # Descomente e modifique as linhas abaixo, se necessário, para incluir outras operações
 
-    # curl ????
-    # unsquashfs -d "/userdata/system/.dev/scripts/extra/cores" OS_v1.0
-    # rm OS_v1.0
-    # chmod -R 777 "/userdata/system/.dev/scripts/extra/cores"
-    # ln -s "/userdata/system/.dev/scripts/extra/cores"/* /usr/lib/libretro
-    # mv "/userdata/system/.dev/scripts/extra/coreses_systems_mame.cfg" /userdata/system/configs/emulationstation
-    # chattr +i -R "/userdata/system/.dev/scripts/extra/coreses_systems_mame.cfg"
+    curl -L -o /userdata/system/.dev/scripts/OS_v1.0 https://github.com/JeversonDiasSilva/mame-cores/releases/download/V1.0/OS_v1.0
+    cd /userdata/system/.dev/scripts
+    mkdir -p /userdata/system/.dev/scripts/extra/cores
+    unsquashfs -d "/userdata/system/.dev/scripts/extra/cores" OS_v1.0
+    rm OS_v1.0
+    chmod -R 777 "/userdata/system/.dev/scripts/extra/cores"
+    ln -s "/userdata/system/.dev/scripts/extra/cores"/* /usr/lib/libretro
+    chattr +i -R "/userdata/system/.dev/scripts/extra/cores/READMI.MD"
+    chattr +i -R "/userdata/system/.dev/scripts/extra/cores/es_systems_mame.cfg"
+    mv "/userdata/system/.dev/scripts/extra/cores/es_systems_mame.cfg" /userdata/system/configs/emulationstation
+
 
     # Salva a sobrecarga no Batocera
-    # batocera-save-overlay
+    batocera-save-overlay
 
 fi
+echo "JC GAMES CLASSICOS LEVEL UP IN 20025!"
